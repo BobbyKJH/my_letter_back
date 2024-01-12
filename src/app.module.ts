ee796d7656f2ee63d2cec4281169e156cea03e9e
bobby <rlatnguscjsw@gmail.com>
2024-01-12 15:03:46 +0900

    Feat : DB Connect
    - DB 연결을 위한 Config.ts 파일 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 /** Cotroller */
 import { AppController } from 'src/app.controller';
 /** Service */
 import { AppService } from 'src/app.service';
 /** Module */
 import { TodoModule } from 'src/todo/todo.module';
+/** 환경 변수 */
+import { typeORMConfig } from 'src/config/typeorm.database.config';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    TodoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
