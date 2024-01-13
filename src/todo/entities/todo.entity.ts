bc1a16113a317c77610c3fb4d917334b7f3eb7b4
bobby <rlatnguscjsw@gmail.com>
2024-01-13 19:13:54 +0900

    Feat : Todo Table 생성
    - Todo Entity 생성

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,1 +1,27 @@
-export class Todo {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+/** Enum */
+import { todoTimeEnum } from "src/todo/const/todo.const";
+
+@Entity("todo")
+export class TodoModel {
+  @PrimaryGeneratedColumn({ type: "int" })
+  id: number;
+
+  @Column({ type: "varchar", length: 100 })
+  title: string;
+
+  @Column("text")
+  content: string;
+
+  @CreateDateColumn()
+  createAt: Date;
+
+  @Column({ type: "int" })
+  writeId: string;
+
+  @Column({ type: "enum", enum: todoTimeEnum })
+  time: string;
+
+  @Column({ type: "int" })
+  view: number;
+}
