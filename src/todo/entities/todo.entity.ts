c096c7c543216fe7d4eeeb6cb511b7b4e65d452d
bobby <rlatnguscjsw@gmail.com>
2024-02-23 13:52:17 +0900

    Feat : Todo Entity
    - writeId One to One User Model

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,6 +1,7 @@
-import { Column, CreateDateColumn, Entity, Generated, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 /** Enum */
 import { todoTimeEnum } from "src/todo/const/todo.const";
+import { UserModel } from "src/user/entities/user.entity";
 
 @Entity("todo")
 export class TodoModel {
@@ -16,7 +17,9 @@ export class TodoModel {
   @CreateDateColumn()
   createAt: Date;
 
-  @Column({ type: "int" })
+  // @Column({ type: "int" })
+  @JoinColumn()
+  @OneToOne(() => UserModel)
   writeId: number;
 
   @Column({ type: "enum", enum: todoTimeEnum })
